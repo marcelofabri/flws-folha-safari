718e5b93ae4c23d6fa635b8bdceffda6fffb3623
Marcelo Fabri <marcelofabrimf@gmail.com>
2014-11-23 03:35:35 -0200

    commit inicial

---
+++ Flws Folha.safariextension/flws_folha.js
@@ -0,0 +1,32 @@
+function remove_paywall(first_time, observer) {
+	document.body.removeAttribute('style');
+	var span = document.getElementsByTagName("span")[0];
+
+	// se tem a span do paywall
+	if (span && span.style && span.style.zIndex === "99999999999") {
+		span.remove();
+		observer.disconnect();
+	}
+
+	// desabilita o setTimeout na pagina, para evitar "travarem" o scroll
+	if (first_time) {
+		var script   = document.createElement("script");
+		script.type  = "text/javascript";
+		script.text  = "window.setTimeout = function() {};"
+		document.body.appendChild(script);
+	}
+}
+
+// We want to support dynamically added content so on every document change
+// do a quick check of the URLs. This will also most likely kick it to action initially.
+
+var observer = new WebKitMutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    remove_paywall(false, this);
+  });    
+});
+
+
+var config = { attributes: true, childList: true, characterData: true, subtree: true };
+observer.observe(document.body, config);
+remove_paywall(true, observer);
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 flws-folha-safari
 =================
+
+Uma extensão para o Safari que remove o paywall (aquele banner enorme) do site da Folha depois que você passa do limite de notícias lidas no mês.
+
+Código baseado no [twitter-urls-to-clients](https://github.com/orta/twitter-urls-to-clients).
+
+## Download: 
+### Version 1.0
+[Safari](releases/Flws Folha-1.0.safariextz)
